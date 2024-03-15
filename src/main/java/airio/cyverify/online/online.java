dbf5cbce6d35e4a0466db5a9b4892d4d414cd732
Alkazar <157254741+alkazargg@users.noreply.github.com>
2024-03-16 00:28:35 +0800

    Update online.java

--- src/main/java/airio/cyverify/online/online.java
+++ src/main/java/airio/cyverify/online/online.java
@@ -24,7 +24,7 @@ public class online implements Listener {
                 public void run() {
                     try {
                         Connection connection = DriverManager.getConnection(jdbc_url, config.getString("mysql.username"),config.getString("mysql.password"));
-                        PreparedStatement statements = connection.prepareStatement("SELECT * FROM `zbcheck` WHERE uuid=?");
+                        PreparedStatement statements = connection.prepareStatement("SELECT * FROM `cyverify` WHERE uuid=?");
                         statements.setString(1,e.getPlayer().getName());
                         ResultSet set = statements.executeQuery();
                         if(set.next()) {
@@ -32,7 +32,7 @@ public class online implements Listener {
                                 e.getPlayer().kickPlayer(config.getString("messages.already-verified"));
                                 return;
                             }
-                            PreparedStatement statement = connection.prepareStatement("UPDATE `zbcheck` SET `verified`='1',`time`=? WHERE `uuid`=?");
+                            PreparedStatement statement = connection.prepareStatement("UPDATE `cyverify` SET `verified`='1',`time`=? WHERE `uuid`=?");
                             statement.setLong(1, System.currentTimeMillis());
                             statement.setString(2, e.getPlayer().getName());
                             statement.executeUpdate();
